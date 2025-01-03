f758d1436bd13ea06db2b7ac8343a40fccb23e16
GuGuGaGa-Star <nik.21daren@gmail.com>
2025-01-04 01:33:48 +0200

    Add files via upload

--- gege2.py
+++ gege2.py
@@ -26,3 +26,15 @@ def do_cropped(self):
     box = (250, 100, 600, 400)
     cropped = self.original.crop(box)
     self.changed.append(cropped)
+
+    temp_filename = self.filename.split('.')
+    new_filename = temp_filename[0] + str(len(self.changed)) + '.jpg'
+
+    cropped.save(new_filename)
+
+MyImage = ImageEditor('original.jpg')
+MyImage.open()
+MyImage.do_left()
+MyImage.do_cropped()
+for im in MyImage.changed:
+    im.show
